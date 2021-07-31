968bb6918ddcef06800ef53162758c7a877e6414
Ivan <example@gmail.com>
2021-07-31 12:24:56 +0300

    script update

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 /*jshint esversion: 6*/
 /*jshint strict: false */
 
-let counter = document.getElementById("chisla"),
+const counter = document.getElementById("chisla"),
     button = document.getElementById("buttoniz"),
     buttons = document.querySelector(".buttons");
-function addElem(){
-    let newButton = document.createElement("div");
-    newButton.className = "button sec";
-    newButton.innerHTML = `<a href="#" onclick="zero" id="buttoniza">Очистить</a>`;
-    buttons.append(newButton);
+let disk = true;
+
+
+function addElement(elemType, hisClass, smthInner, fromWhere){
+    let ovvvee = document.createElement(elemType);
+    ovvvee.className = hisClass;
+    ovvvee.innerHTML = smthInner;
+    fromWhere.append(ovvvee);
 }
 
-let disk = true;
+
 
 button.onclick = function(){
     counter.innerText = +counter.innerText + 1;
-    if(+counter.innerText>0){
-        if(disk){
-            addElem();
-            disk = false;
-        }
+    if(+counter.innerText>0 && disk){
+        addElement("div", "button", `<a href="#" id="buttoniza">Очистить</a>`, buttons );
+        disk = false;
     }
+
     let a = document.getElementById("buttoniza");
     a.onclick = function(){
         counter.innerText = "0";
